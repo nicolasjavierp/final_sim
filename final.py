b1a1eb503b493dd92f61c50d0c0da091951befb1
Nico Fantaxis <nicospider2002@yahoo.com.ar>
2018-12-24 17:53:44 -0300

    Unified comments language

--- final.py
+++ final.py
@@ -18,48 +18,6 @@ import scipy as SP
 import copy as cp
 
 
-########################################################################################################
-#####################################    Mathematic Representation     #################################
-########################################################################################################
-
-
-#Common Formulas
-h=1 # Number of Predators currently harvesting prey
-s=1-h # Fraction of predator population searching for prey
-Ts=5 # Time until predator finds patch of prey 
-Th=5 # Time until predator returns to searching
-
-#Formulas Single-predator
-H=5 #Consuption rate prey/time
-H_max=5 #Consuption rate prey/time when s=0 abundant prey
-proportion_H_alone = 1/(1+Ts/Th) # H/H_max
-
-
-#Formulas Social-predators
-Tl=5 # Time until prey patch can be exploited
-Td=5 # Time until predator reaches another predator
-N=5 # Number of predators hunting in the same area
-lambda_share=5 # Propensity of sharing information
-m=5 # Number of predators moving toward a broadcasted prey patch 
-b=5 # Number of predators reached the patch with broadcaster
-proportion_H_social = h+b # H/H_max
-Wsh=1/Ts # Rate which lone predators find and start consuming prey patch
-Wsm=N*lambda_share*s*Wsh # Rate which lone predators get the broadcast
-Wmb=1/Td # Rate which lone predators reach the broadcaster
-
-limit=0.3
-#np split function
-if lambda_share>limit:
-    np=N
-else:
-    np=1+b/h
-
-WL= 1/Tl + Th/np # Rate which a predator is interupted while exploiting patch (depleation or movement of patch)
-
-#Whs=Wms=Wbs=WL => Rate which predators revert to searching alone
-Whs = WL
-Wms = Whs
-Wbs = Wms
 
 
 ########################################################################################################
@@ -91,7 +49,6 @@ sharing = False
 def distance_between_2_points(x1,y1,x2,y2):
     dist = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)  
     return dist
-
 
 class agent:
     pass
@@ -219,6 +176,7 @@ def update():
             # detecting collision
             neighbors = [nb for nb in agents if nb.type != ag.type and (ag.x - nb.x)**2 + (ag.y - nb.y)**2 < cdsq]
             # we assume we got to the same patch as the predator that broadcasted
+            #TODO fix above
             if len(neighbors) > 0:
                 ag.current_state="consuming_with_friend"
             else:
@@ -229,7 +187,7 @@ def update():
             #No movement
             neighbors = [nb for nb in agents if nb.type != ag.type and (ag.x - nb.x)**2 + (ag.y - nb.y)**2 < cdsq]
             if len(neighbors) > 0: # if there are rabbits nearby
-                # consumimos del primer parche en la lista
+                # Fox consumes first patch of the list
                 nb=neighbors[0]
                 nb.patch_population_curr=nb.patch_population_curr-consumption_rate
                 ag.broadcast=True
@@ -259,12 +217,13 @@ def update():
         return
     else: # not sharing information
         if ag.type=='f' and ag.current_state=="searching":
-            # esto mueve en un CUADRADO de m*m, no en un CIRCULO
+            # this represents a movement in a m*m SQUARE , not circular
             ag.x += uniform(-m, m)
             ag.y += uniform(-m, m)
-            # esto limita a 1 valores mayores a 1 y a 0 valores menores a 0 (NO CICLICO)
+            # this limits to 1 values larger than 1 and 0 to values smaller than 0, not circular
             ag.x = 1 if ag.x > 1 else 0 if ag.x < 0 else ag.x
             ag.y = 1 if ag.y > 1 else 0 if ag.y < 0 else ag.y
+            
             # detecting collision
             neighbors = [nb for nb in agents if nb.type != ag.type and (ag.x - nb.x)**2 + (ag.y - nb.y)**2 < cdsq]
             if len(neighbors) > 0: # if there are rabbit nearby
@@ -313,3 +272,54 @@ def update_one_unit_time():
 
 import pycxsimulator
 pycxsimulator.GUI().start(func=[initialize, observe, update_one_unit_time])
+
+
+
+
+
+
+
+
+
+########################################################################################################
+#####################################    Mathematic Representation     #################################
+########################################################################################################
+
+
+#Common Formulas
+#h=1  Number of Predators currently harvesting prey
+#s=1-h  Fraction of predator population searching for prey
+#Ts= Time until predator finds patch of prey 
+#Th= Time until predator returns to searching
+
+#Formulas Single-predator
+#H=#Consuption rate prey/time
+#H_max=#Consuption rate prey/time when s=0 abundant prey
+#proportion_H_alone = 1/(1+Ts/Th) # H/H_max
+
+
+#Formulas Social-predators
+#Tl=# Time until prey patch can be exploited
+#Td=# Time until predator reaches another predator
+#N=# Number of predators hunting in the same area
+#lambda_share=5 # Propensity of sharing information
+#m=# Number of predators moving toward a broadcasted prey patch 
+#b=# Number of predators reached the patch with broadcaster
+#proportion_H_social = h+b # H/H_max
+#Wsh=1/Ts # Rate which lone predators find and start consuming prey patch
+#Wsm=N*lambda_share*s*Wsh # Rate which lone predators get the broadcast
+#Wmb=1/Td # Rate which lone predators reach the broadcaster
+
+#limit=#np split function
+
+#if lambda_share>limit:
+#    np=N
+#else:
+#    np=1+b/h
+
+#WL= 1/Tl + Th/np # Rate which a predator is interupted while exploiting patch (depleation or movement of patch)
+
+#Whs=Wms=Wbs=WL => Rate which predators revert to searching alone
+#Whs = WL
+#Wms = Whs
+#Wbs = Wms
